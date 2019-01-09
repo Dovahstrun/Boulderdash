fedafccd391467fdad4759543690b985ca03b44a
Dovahstrun <43002140+Dovahstrun@users.noreply.github.com>
2019-01-09 17:12:53 +0000

    Added in 3 new levels

--- Game Code/Sources/Boulder.cpp
+++ Game Code/Sources/Boulder.cpp
@@ -2,6 +2,7 @@
 #include "../../Framework/Headers/AssetManager.h"
 #include "../Headers/Level.h"
 #include "../Headers/Player.h"
+#include "../Headers/Diamond.h"
 
 Boulder::Boulder()
 	: GridObject()
@@ -127,6 +128,60 @@ bool Boulder::AttemptFall(sf::Vector2i _direction)
 
 		//If so, attempt to fall (the blocker is a boulder, not nullptr)
 		if (landBoulder != nullptr && m_hasFallen)
+		{
+			///Need to check if either the cell to the left or right is blocked. Check the left first, and if the left is blocked, then check right
+			///If either cell is unblocked, move there, and continue falling
+
+			//Check if the cell to the right block movement
+			//If not, move there
+			sf::Vector2i newTargetPos = m_gridPosition;
+			newTargetPos.x = m_gridPosition.x + 1;
+			targetCellContents = m_level->getObjectAt(newTargetPos);
+			blocked = false;
+			//Check if any of those objects block movement
+			for (int i = 0; i < targetCellContents.size(); ++i)
+			{
+				if (targetCellContents[i]->getBlocksMovement() == true)
+				{
+					blocked = true;
+				}
+			}
+
+			//If the cell to the left is not blocked, move there
+			if (!blocked)
+			{
+				m_hasFallen = false;
+				return m_level->MoveObjectTo(this, newTargetPos);
+			}
+			else
+			{
+				//Check if the cell to the right block movement
+				//If not, move there
+				newTargetPos.x = m_gridPosition.x - 1;
+				targetCellContents = m_level->getObjectAt(newTargetPos);
+				blocked = false;
+				//Check if any of those objects block movement
+				for (int i = 0; i < targetCellContents.size(); ++i)
+				{
+					if (targetCellContents[i]->getBlocksMovement() == true)
+					{
+						blocked = true;
+					}
+				}
+
+				//If the cell to the left is not blocked, move there
+				if (!blocked)
+				{
+					m_hasFallen = false;
+					return m_level->MoveObjectTo(this, newTargetPos);
+				}
+			}
+		}
+
+		Diamond* landDiamond = dynamic_cast<Diamond*>(blocker);
+
+		//If so, attempt to fall (the blocker is a boulder, not nullptr)
+		if (landDiamond != nullptr && m_hasFallen)
 		{
 			///Need to check if either the cell to the left or right is blocked. Check the left first, and if the left is blocked, then check right
 			///If either cell is unblocked, move there, and continue falling
